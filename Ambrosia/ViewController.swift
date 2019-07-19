87a0c2206d9edc61fd331e3ae97f293238984e1c
Marcel <henning_marcel@web.de>
2019-07-19 05:35:59 -0700

    Initial Commit

---
+++ Ambrosia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ambrosia
+//
+//  Created by Marcel on 19.07.19.
+//  Copyright © 2019 Marcel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AmbrosiaTests/AmbrosiaTests.swift
@@ -0,0 +1,34 @@
+//
+//  AmbrosiaTests.swift
+//  AmbrosiaTests
+//
+//  Created by Marcel on 19.07.19.
+//  Copyright © 2019 Marcel. All rights reserved.
+//
+
+import XCTest
+@testable import Ambrosia
+
+class AmbrosiaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
